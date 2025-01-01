19686239643731b531eaebb0c9038413e43034b4
saharankeshav <saharankeshav747@gmail.com>
2025-01-02 01:02:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// Retrieve todo from local storage or initialias an empty array
+
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+
+const todoInput = document.getElementById("todoInput");
+const todoList =  document.getElementById("todoList");
+const todoCount =  document.getElementById("todoCount");
+const addButton =  document.querySelector(".btn");
+const deleteButton =  document.getElementById("deleteButton");
+
+//Initialize
+
+document.addEventListener("DOMContentLoaded" , function () {
+    addButton.addEventListener("click" , addTask);
+    todoInput.addEventListener("keydown" , function(event) {
+        if (event.key === "Enter"){
+            event.preventDefault();
+            addTask();
+        } 
+    });
+    deleteButton.addEventListener("click" , deleteAllTasks);
+    displayTasks();
+});
+
+function addTask() {
+    const newTask = todoInput.value.trim();
+    if(newTask !== "" ){
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        savetoLocalStorage();
+        todoInput.value = "";
+        displayTasks();
+    }
+
+}
+
+function displayTasks() {
+    todoList.innerHTML = "";
+    todo.forEach((item, index) => {
+      const p = document.createElement("p");
+      p.innerHTML = `
+        <div class="todo-container">
+          <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
+        item.disabled ? "checked" : ""
+      }>
+          <p id="todo-${index}" class="${
+        item.disabled ? "disabled" : ""
+      }" onclick="editTask(${index})">${item.text}</p>
+        </div>
+      `;
+      p.querySelector(".todo-checkbox").addEventListener("change", () =>
+        toggleTask(index)
+      );
+      todoList.appendChild(p);
+    });
+    todoCount.textContent = todo.length;
+  }
+
+function toggleTask(index) {
+    todo[index].disabled = !todo[index].disabled; 
+    savetoLocalStorage();
+    displayTasks();
+};
+
+function deleteAllTasks() {
+    todo = [];
+    savetoLocalStorage();
+    displayTasks();
+}
+
+function savetoLocalStorage() {
+    localStorage.setItem("todo" , JSON.stringify(todo));
+};
+
